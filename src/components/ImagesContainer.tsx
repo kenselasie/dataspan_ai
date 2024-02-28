3b6f112dce617dcdab71b8363d04b6b975920df7
kenselasie <kenselasie@gmail.com>
2024-02-28 19:32:50 +0000

    update:

--- src/components/ImagesContainer.tsx
+++ src/components/ImagesContainer.tsx
@@ -4,11 +4,12 @@ import { BoneFractureType } from "@/app/page";
 import Image from "next/image";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { useSearchParams } from "next/navigation";
-
 type ImagesContainerType = {
   data: BoneFractureType[];
 };
 const ImagesContainer = ({ data }: ImagesContainerType) => {
+  const href =
+    "https://s3.eu-central-1.amazonaws.com/dataspan.frontend-home-assignment/bone-fracture-detection";
   const searchParams = useSearchParams();
   const mode = searchParams.get("mode") || "";
   const [openDialog, setOpendialog] = React.useState(false);
@@ -16,9 +17,15 @@ const ImagesContainer = ({ data }: ImagesContainerType) => {
   const [currentPage, setCurrentPage] = React.useState(1);
   const itemsPerPage = 50;
 
+  const classAttr = {
+    classId: {
+      color: "red",
+    },
+  };
   const showImageModal = (data: BoneFractureType) => {
     setOpendialog(true);
     setModalData(data);
+    // drawPolygon();
   };
 
   const indexOfLastItem = currentPage * itemsPerPage;
@@ -30,6 +37,25 @@ const ImagesContainer = ({ data }: ImagesContainerType) => {
     setCurrentPage(pageNumber);
   };
 
+  function scaleCoordinates(
+    coordinates: string,
+    imageWidth: number,
+    imageHeight: number
+  ): string {
+    const coordinatePairs = coordinates.split(" ");
+
+    const scaledCoordinates = coordinatePairs.map((pair) => {
+      const [x, y] = pair.split(",");
+
+      const scaledX = parseFloat(x) * imageWidth;
+      const scaledY = parseFloat(y) * imageHeight;
+
+      return `${scaledX},${scaledY}`;
+    });
+
+    return scaledCoordinates.join(" ");
+  }
+
   React.useEffect(() => {
     paginate(1);
   }, [mode]);
@@ -39,15 +65,32 @@ const ImagesContainer = ({ data }: ImagesContainerType) => {
       <Dialog open={openDialog} onOpenChange={() => setOpendialog(false)}>
         <div className="grid grid-cols-10 items-center gap-4 w-max">
           {currentItems?.map((el, i) => (
-            <div key={i}>
+            <div
+              className="relative w-full"
+              onClick={() => showImageModal(el)}
+              key={i}
+            >
               <Image
                 src={el.thumbnail}
                 alt={el.labels}
                 className="w-[100px] h-[100px]"
                 width={100}
                 height={100}
-                onClick={() => showImageModal(el)}
               />
+              <svg
+                width="100%"
+                height="100%"
+                viewBox="0 0 1 1"
+                style={{ position: "absolute", top: 0, left: 0 }}
+              >
+                <polygon
+                  width={10}
+                  points={scaleCoordinates(el.labels, 100, 100)}
+                  fill={classAttr.classId?.color}
+                  stroke="red"
+                  strokeWidth={2}
+                />
+              </svg>
             </div>
           ))}
         </div>
@@ -55,10 +98,24 @@ const ImagesContainer = ({ data }: ImagesContainerType) => {
           {modalData && (
             <div>
               <div className="sm:max-w-[400px]">
-                {/* <p className="text-wrap break-normal">{decodeURIComponent(modalData.image.replace(href, ''))}</p> */}
-                <p className="text-wrap break-normal">{modalData.labels}</p>
+                
+                <p className="text-wrap break-normal">{modalData.labels.substring(2)}</p>
               </div>
-              <div className="flex flex-col items-center m-5">
+              <p className="text-wrap break-normal truncate">
+                  {decodeURIComponent(modalData.image)
+                    .replace(href, "")
+                    .replace("/valid/images/", "")
+                    .replace("/test/images/", "")
+                    .replace("/train/images/", "")}
+                </p>
+              <div className="relative w-full flex flex-col items-center m-5">
+
+                <div className="self-start">
+                  <p>Details:</p>
+                  <button className="px-5 mb-4  rounded-3xl bg-yellow-400">
+                    fracture_1
+                  </button>
+                </div>
                 <Image
                   src={modalData.image}
                   alt={modalData.image}
@@ -66,10 +123,23 @@ const ImagesContainer = ({ data }: ImagesContainerType) => {
                   width={100}
                   height={100}
                 />
-                <canvas
-                  id="overlayCanvas"
-                  className="absolute top-0 left-0 w-[500px] h-[500px]"
-                ></canvas>
+                <svg
+                  width="100%"
+                  height="100%"
+                  viewBox="0 0 1 1"
+                  style={{ position: "absolute", top: 0, left: 0 }}
+                >
+                  <polygon
+                    points={scaleCoordinates(
+                      modalData.labels.substring(2),
+                      500,
+                      500
+                    )}
+                    fill={classAttr.classId?.color}
+                    stroke="red"
+                    strokeWidth={2}
+                  />
+                </svg>
               </div>
             </div>
           )}
