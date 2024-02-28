c87d433ed1e3c5c180fc880e0955d8de3acffcfe
kenselasie <kenselasie@gmail.com>
2024-02-28 16:31:14 +0000

    progress

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import logo from "@/assets/logo.svg";
-import { Dot, Trash, Trash2 } from "lucide-react";
-
+import { Dot, Trash2 } from "lucide-react";
+import progressImg from "@/assets/progress.svg";
 const Navigation = ({ data }: { data: string[] }) => {
   return (
     <aside className="w-[332px] border rounded-lg m-7">
@@ -21,7 +21,9 @@ const Navigation = ({ data }: { data: string[] }) => {
               <div className="" key={i}>
                 <div className="flex items-center border-2 border-[#3D9BE9] w-max py-1 pr-2 rounded-3xl my-2 bg-[#b9d8f2]">
                   <Dot color="#3D9BE9" spacing={0} strokeWidth={8} />{" "}
-                  <p className="text-sm font-semibold capitalize-first-letter">{data}</p>
+                  <p className="text-sm font-semibold capitalize-first-letter">
+                    {data}
+                  </p>
                 </div>
               </div>
             ))}
@@ -37,7 +39,9 @@ const Navigation = ({ data }: { data: string[] }) => {
               max <span className="font-semibold">4</span>
             </p>
           </div>
-          <div className="mt-3">divrogress bar</div>
+          <div className="mt-3">
+            <Image src={progressImg} alt="progress"/>
+          </div>
           <div className="flex items-center justify-between text-sm mt-6">
             <div className="flex items-center gap-1 font-semibold">
               <Trash2 /> <p>Clear Filters</p>
